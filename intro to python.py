899536206c8401f3a08d6585f66321f1368eb2f6
harshi0102 <108334376+harshi0102@users.noreply.github.com>
2022-11-07 12:55:13 +0530

    Create intro to python.py

---
+++ intro to python.py
@@ -0,0 +1,24 @@
+# cOMMENTS USED TO EXPLAIN THE FUNCTIONING OF A BLOCK OF A CODE .iT IS DENOTED BY #
+#BLOCK OF A CODE IS IDENTIIFIED BY THE INDENTATION. For example below:
+
+for i in range(5):
+  print(i)   #print i is said to be indented with respect to the link above it. in other words, identation is the addition of spaces just before the  print(i) 
+  
+  #python comments
+  #comments are part of coding file which programmer does not want to execute,or programmer use comments to explain the block of code or to avoid the execution of specific part of a code while testing
+  
+ #single line comments
+print("this is a print statement")
+
+#multiline comments
+#you can use # multitimes or you can use multilinne string
+'''this is an if else statement
+it will execute a block of code if a specified condition is true.
+if the condition is true, it will execute another block of code.'''
+p=7
+if (p>5)
+print("print p is greater than 5")
+else:
+  print("p is not greater than 5")
+  
+ 
